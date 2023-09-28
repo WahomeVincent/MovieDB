92b1a2ddfbaba0186387b6c212edb17237dbe4e3
Wahome <vinwahome@gmail.com>
2023-09-28 20:01:11 +0300

    Style the navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './navbar.scss'
+import { ArrowDropDown, Notifications, Search } from '@mui/icons-material'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="container">
+            <div className="left">
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png' alt='Netflix-logo' />
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New and Popular</span>
+                <span>My List</span>
+            </div>
+            <div className="right">
+                <Search  className='icon'/>
+                <span>KID</span>
+                <Notifications className='icon'/>
+                <img src='https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&w=400' alt='profile' />
+                <div className="profile">
+                    <ArrowDropDown className='icon' />
+                    <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,97 @@
 import React from 'react'
-import './Home.scss'
+import './home.scss'
+import Navbar from '../components/navbar/Navbar'
 
 export default function Home() {
   return (
     <div className='home'>
-        Hello
+        <Navbar />
+        <img 
+        width='100%'
+        src='https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=400' alt='profile' />
+
+        <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nisi, molestiae aliquid deserunt laudantium saepe quasi cum dicta vitae inventore consequatur! Quam labore itaque neque sequi blanditiis voluptatem vitae odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloremque in ad repellat odio architecto rem debitis velit consequatur veniam omnis, corrupti ipsa consequuntur? Explicabo dolores earum dicta quas numquam!
+      </div>
     </div>
   )
 }
