2264d7fa5595f60af843cdf4b680936369c3edd5
Wahome <vinwahome@gmail.com>
2023-09-29 20:59:26 +0300

    Write and design the featured property

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './featured.scss'
+import { InfoOutlined, PlayArrow } from '@mui/icons-material'
+
+export default function Featured(type) {
+  return (
+    <div className='featured'>
+        {type && (
+            <div className="category">
+                <span>{type === "movie" ? "Movies" : "Series"}</span>
+                <select name="genre" id="genre">
+                    <option>Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentary">Documentary</option>
+                </select>
+                    
+            </div>
+        )}
+        <img 
+            src='https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&w=400'
+            alt='feature'
+        />
+        <div className="info">
+            <img 
+                src="https://images.unsplash.com/photo-1675181759397-ea032114694d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDl8aG1lbnZRaFVteE18fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                alt="info" />
+        
+            <span className="desc">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, temporibus doloremque qui itaque animi consequatur aliquid deleniti, voluptates repudiandae ut totam recusandae quae eius iste quam consequuntur vitae ullam accusamus!
+            </span>
+            <div className="buttons">
+                <button className="play">
+                    <PlayArrow />
+                    <span>Play</span>
+                </button>
+                <button className="more">
+                    <InfoOutlined />
+                    <span>Info</span>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
