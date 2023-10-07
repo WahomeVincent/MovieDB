67ac77fbc7035038e3f29253a753226c570efca7
Wahome <vinwahome@gmail.com>
2023-10-07 15:15:39 +0300

    Write the logic for the sliding tiles(movies)

---
+++ src/components/list/List.jsx
@@ -0,0 +1,42 @@
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
+import React from './list.scss'
+import ListItem from '../listItem/ListItem'
+import { useRef } from 'react'
+
+export default function List() {
+
+  const listRef = useRef()
+
+  function handleClick(direction){
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === 'left'){
+       listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+
+    if(direction === 'right'){
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`
+   }
+    
+  }
+
+  return (
+    <div className='list'>
+        <span className="list-title">Continue to watch</span>
+        <div className="wrapper">
+            <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick('left')} />
+            <div className="container" ref={listRef}>
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                
+            </div>
+            <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick('right')} />
+        </div>
+
+    </div>
+  )
+}
